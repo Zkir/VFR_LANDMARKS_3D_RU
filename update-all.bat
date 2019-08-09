eaa8d47441c61603a8339945155bcd90ae84625f
Kirill "Zkir <mekillot@yandex.ru>
2019-08-09 17:56:26 +0300

    small fixes

--- update-all.bat
+++ update-all.bat
@@ -4,5 +4,5 @@ rem update +55+037 "37,55,38,56"
 rem update +52+041 41,52,42,53
 call update +59+030 "30,59,31,60"
 call extract +59+030
-OsmParser/main.py +59+030
-OsmParser/test.py +59+030
+OsmParser\main.py +59+030
+OsmParser\test.py +59+030
