98969cc7126a23dcfda1cab1f225119c90154b68
Kirill "Zkir <mekillot@yandex.ru>
2019-10-03 18:13:45 +0300

    Test2: saving to poly

---
+++ OsmParser/test2.py
@@ -0,0 +1,25 @@
+﻿# This is our playground
+# here we will check our ideas.
+from mdlSite import *
+from mdlGeocoder import *
+from mdlDBMetadata import *
+
+MIN_DATE='1900.01.01 00:00:00'
+
+
+print("Loading geocoder...")
+t3 = time.time()
+geocoder = Geocoder()
+geocoder.loadDataFromTextFile("d:\\_planet.osm\\geocoder.txt")
+t4 = time.time()
+print("Geocoder loaded in " + str(t4 - t3) + " seconds")
+
+print("Saving poly")
+#geocoder.saveDataToPolyFile("d:\\RU-AD.poly","253256")
+#geocoder.saveDataToPolyFile("d:\\RU-KDA.poly","108082")
+geocoder.saveDataToPolyFile("d:\\RU-SPE.poly","337422")
+geocoder.saveDataToPolyFile("d:\\RU-LEN.poly","176095")
+
+print("Done!")
+
+
