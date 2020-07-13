0c38eb46882d64cad6c5acf5ac7c91e3a3a614b4
Kirill "Zkir <mekillot@yandex.ru>
2020-07-14 02:51:12 +0300

    refactoring
    Operations moved to own functions

--- OsmParser/testTransform.py
+++ OsmParser/testTransform.py
@@ -1,21 +1,170 @@
-﻿from mdlOsmParser import T3DObject,readOsmXml, writeOsmXml
+﻿from mdlOsmParser import T3DObject,readOsmXml, writeOsmXml, parseHeightValue
 from mdlXmlParser import encodeXmlString
 from osmGeometry import DEGREE_LENGTH_M
 from copy import copy
-from math import cos, sin,pi
+from math import cos, sin, pi
 
 _id_counter=0
+
+
+# counter for OSM objects.
+# we need negative, because those objects are absent (yet) in OSM DB
 def getID():
     global _id_counter
     _id_counter=_id_counter-1
     return str(_id_counter)
 
 
+# ======================================================================================================================
+# Operations with building parts.
+# ======================================================================================================================
 
+
+# split object in vertical direction.
+# since we have only 2.5, it's easy
+# outline and tags remain,
+# min_height and height are assigned to the new parts.
+def split_z_preserve_roof(osmObject, split_pattern):
+    Objects2 = []
+    N = len(split_pattern)
+    height= parseHeightValue(osmObject.getTag("height"))
+    min_height = parseHeightValue(osmObject.getTag("min_height"))
+    roof_height= parseHeightValue(osmObject.getTag("roof:height"))
+    h = height-min_height-roof_height # osm tags are strange. Split ignores roof height
+
+    # we need to calculate heights of the segments
+    # it is a bit tricky, because it could be "floating" height
+    Heights=[None]* N
+    sum_of_explicit_heights=0
+    sum_of_implicit_heights=0
+    for i in range(N):
+        if split_pattern[i][0][0:1]!="~": # height starts with '~'
+            sum_of_explicit_heights=sum_of_explicit_heights+float(split_pattern[i][0])
+            Heights[i] = float(split_pattern[i][0])
+        else:
+            sum_of_implicit_heights = sum_of_implicit_heights + float(split_pattern[i][0][1:])
+    # define values for floating segments proportionally
+    for i in range(N):
+        if split_pattern[i][0][0:1] != "~":  # height starts with '~'
+            Heights[i] = float(split_pattern[i][0])
+        else:
+            Heights[i] = (h-sum_of_explicit_heights) *float(split_pattern[i][0][1:])/sum_of_implicit_heights
+
+        if Heights[i]<0: # todo: more precise check for negative height. Such elements probably should be excluded from generaion.
+            Heights[i]=0
+    print(Heights)
+
+    for i in range(N) :
+        # create new object and copy outline and tags
+        # it does not depend on split pattern in case of Z split
+        new_obj = T3DObject()
+        new_obj.id = getID()
+        new_obj.type = osmObject.type
+        new_obj.NodeRefs = copy(osmObject.NodeRefs)
+        new_obj.WayRefs = copy(osmObject.WayRefs)
+        new_obj.osmtags = copy(osmObject.osmtags)
+        new_obj.bbox = copy(osmObject.bbox)
+        new_obj.size = osmObject.size
+        # we can assign building part tag, it is identical with rule name
+        new_obj.osmtags["building:part"] = split_pattern[i][1]
+        if i!=N-1:
+            new_obj.osmtags["roof:shape"] = "flat"  # No roof for lower parts, roof shape remains for top-most part only
+            new_obj.osmtags["roof:height"] = "0"
+
+        new_obj.osmtags["min_height"] = str(min_height)
+        if i != N - 1:
+           new_obj.osmtags["height"] = str(min_height+Heights[i])
+        else:
+            pass
+            new_obj.osmtags["height"] = str(min_height + Heights[i]+roof_height)
+        min_height=min_height+Heights[i]
+
+        Objects2.append(new_obj)
+
+    return Objects2
+
+
+#todo: split pattern
+def split_x(osmObject, objOsmGeom, rule_name, n):
+    Objects2 = []
+    scope_sx = osmObject.scope_sx
+    scope_sy = osmObject.scope_sy
+
+    n = 4
+    # n = round(scope_sx/scope_sy/2)*2
+    dx = scope_sx / n
+    #print("scope:", scope_sx, scope_sy, n, dx)
+    for i in range(n):
+        new_obj = T3DObject()
+        new_obj.id = getID()
+        new_obj.type = "way"
+
+        new_obj.osmtags["building:part"] = rule_name
+        new_obj.osmtags["height"] = osmObject.getTag("height")
+        new_obj.osmtags["min_height"] = osmObject.getTag("min_height")
+
+        # 1
+        Lat, Lon = osmObject.localXY2LatLon(-scope_sx / 2 + dx * i, -scope_sy / 2)
+        node_no_1 = objOsmGeom.AddNode(getID(), Lat, Lon)
+        new_obj.NodeRefs.append(node_no_1)
+
+        # 2
+        Lat, Lon = osmObject.localXY2LatLon(-scope_sx / 2 + dx * (i + 1), -scope_sy / 2)
+        new_obj.NodeRefs.append(objOsmGeom.AddNode(getID(), Lat, Lon))
+
+        # 3
+        Lat, Lon = osmObject.localXY2LatLon(-scope_sx / 2 + dx * (i + 1), +scope_sy / 2)
+        new_obj.NodeRefs.append(objOsmGeom.AddNode(getID(), Lat, Lon))
+
+        # 4
+        Lat, Lon = osmObject.localXY2LatLon(-scope_sx / 2 + dx * (i), +scope_sy / 2)
+        new_obj.NodeRefs.append(objOsmGeom.AddNode(getID(), Lat, Lon))
+
+        # 5
+        new_obj.NodeRefs.append(node_no_1)
+        new_obj.updateBBox(objOsmGeom)
+
+        Objects2.append(new_obj)
+
+    return Objects2
+
+#todo: where the tags are inherited and modified
+def insert_circle(osmObject,objOsmGeom):
+    Objects2=[]
+    new_obj = T3DObject()
+    new_obj.id = getID()
+    new_obj.type = "way"
+
+    new_obj.osmtags["building:part"] = "porch_column"
+    new_obj.osmtags["height"] = osmObject.getTag("height")
+    new_obj.osmtags["min_height"] = osmObject.getTag("min_height")
+
+    new_obj.osmtags["building:colour"] = osmObject.getTag("building:colour")
+
+    R = osmObject.size / 3
+    Lat = [None] * 12
+    Lon = [None] * 12
+    ids = [None] * 12
+
+    for i in range(12):
+        alpha = 2 * pi / 12 * i
+
+        Lat[i], Lon[i] = osmObject.localXY2LatLon(R * cos(alpha), R * sin(alpha))
+        ids[i] = getID()
+        # print(ids[i], x[i], y[i])
+        objOsmGeom.AddNode(ids[i], Lat[i], Lon[i])
+        intNodeNo = objOsmGeom.FindNode(ids[i])
+        new_obj.NodeRefs.append(intNodeNo)
+    # objOsmGeom.AddNode(ids[0], Lat[0], Lon[0])
+    intNodeNo = objOsmGeom.FindNode(ids[0])
+    new_obj.NodeRefs.append(intNodeNo)
+
+    Objects2.append(new_obj)
+    return Objects2
+# ======================================================================================================================
 def main():
     #objOsmGeom, Objects = readOsmXml("d:\_BLENDER-OSM-TEST\samples\Church-vozdvizhenskoe.osm")
-    objOsmGeom, Objects = readOsmXml("d:\original.osm")
-    #print(len(Objects))
+    objOsmGeom, Objects = readOsmXml("d:\\original.osm")
 
     #magic!
     blnThereAreUnprocessedRules = True
@@ -28,54 +177,11 @@ def main():
 
                 #we want to remove it, and replace with 3 orther objects: porch_base, porch_columns, porch_top
                 #Split Z, preserve roof,  {1:porch_base| ~1:porch_columns | 1: porch_top}
+                new_objects = split_z_preserve_roof (osmObject, (("1", "porch_base"),
+                                                                 ("~5", "porch_columns"),
+                                                                 ("1", "porch_top")))
 
-                # Porch Base
-                porch_base=T3DObject()
-                porch_base.id=getID()
-                porch_base.type = osmObject.type
-                porch_base.NodeRefs = copy(osmObject.NodeRefs)
-                porch_base.WayRefs = copy(osmObject.WayRefs)
-                porch_base.osmtags = copy(osmObject.osmtags)
-                porch_base.bbox = copy(osmObject.bbox)
-                porch_base.size = osmObject.size
-
-                porch_base.osmtags["building:part"] = "porch_base"
-                porch_base.osmtags["height"] = "1"
-                porch_base.osmtags["roof:shape"] ="flat" # No roof for lower parts, roof shape remains for top-most part only
-                porch_base.osmtags["roof:height"] = "0"
-
-                # Columns
-                porch_columns=T3DObject()
-                porch_columns.id = getID()
-                porch_columns.type = osmObject.type
-                porch_columns.NodeRefs = copy(osmObject.NodeRefs)
-                porch_columns.WayRefs = copy(osmObject.WayRefs)
-                porch_columns.osmtags=copy(osmObject.osmtags)
-                porch_columns.bbox=copy(osmObject.bbox)
-                porch_columns.size=osmObject.size
-
-                porch_columns.osmtags["building:part"] = "porch_columns"
-                porch_columns.osmtags["min_height"] = "1"
-                porch_columns.osmtags["height"] = str(float (osmObject.osmtags["height"])-float(osmObject.osmtags["roof:height"]) -1)   #"5"
-                porch_columns.osmtags["roof:shape"] = "flat"
-                porch_columns.osmtags["roof:height"] = "0"
-
-                # Porch Top
-                porch_top=T3DObject()
-                porch_top.id=getID()
-                porch_top.type = osmObject.type
-                porch_top.NodeRefs = copy(osmObject.NodeRefs)
-                porch_top.WayRefs = copy(osmObject.WayRefs)
-                porch_top.osmtags = copy(osmObject.osmtags)
-                porch_top.bbox = copy(osmObject.bbox)
-                porch_top.size = osmObject.size
-
-                porch_top.osmtags["building:part"] = "porch_top"
-                porch_top.osmtags["min_height"] = str(float (osmObject.osmtags["height"])-float(osmObject.osmtags["roof:height"]) -1)
-
-                Objects2.append(porch_base)
-                Objects2.append(porch_columns)
-                Objects2.append(porch_top)
+                Objects2.extend(new_objects)
 
                 blnThereAreUnprocessedRules = True
 
@@ -84,98 +190,25 @@ def main():
                 Objects2.append(osmObject)
 
             elif osmObject.getTag("building:part") == "porch_columns":
-                # split(x){ {~sy:porch_column_pre| ~sy:Nil}* | ~sy:porch_column_pre }
-                #osmObject.osmtags["building:colour"] = "pink"
 
                 #osmObject.alignScopeToWorld()
                 osmObject.alignScopeToGeometry(objOsmGeom)
-                #osmObject.rotateScope(33, objOsmGeom)
 
                 if osmObject.scope_sx<osmObject.scope_sy:
                     osmObject.rotateScope(90, objOsmGeom)
 
-                scope_sx = osmObject.scope_sx
-                scope_sy = osmObject.scope_sy
+                # split(x){ {~sy:porch_column_pre| ~sy:Nil}* | ~sy:porch_column_pre }
+                new_objects = split_x(osmObject,objOsmGeom,"porch_column_pre",4)
 
-                n = 4
-                # n = round(scope_sx/scope_sy/2)*2
-                dx = scope_sx/n
-                print("scope:", scope_sx, scope_sy, n, dx)
-                for i in range (n):
-                    porch_column_pre = T3DObject()
-                    porch_column_pre.id = getID()
-                    porch_column_pre.type = "way"
-
-                    porch_column_pre.osmtags["building:part"] = "porch_column_pre"
-                    porch_column_pre.osmtags["height"] = osmObject.getTag("height")
-                    porch_column_pre.osmtags["min_height"] = osmObject.getTag("min_height")
-
-                    # 1
-                    Lat, Lon = osmObject.localXY2LatLon(-scope_sx/2+dx*i,-scope_sy/2)
-                    node_no_1=objOsmGeom.AddNode(getID(), Lat, Lon)
-                    porch_column_pre.NodeRefs.append(node_no_1)
-
-                    # 2
-                    Lat, Lon = osmObject.localXY2LatLon(-scope_sx / 2 + dx * (i+1), -scope_sy / 2)
-                    porch_column_pre.NodeRefs.append(objOsmGeom.AddNode(getID(), Lat, Lon))
-
-                    # 3
-                    Lat, Lon = osmObject.localXY2LatLon(-scope_sx / 2 + dx * (i+1), +scope_sy / 2)
-                    porch_column_pre.NodeRefs.append(objOsmGeom.AddNode(getID(), Lat, Lon))
-
-                    # 4
-                    Lat, Lon = osmObject.localXY2LatLon(-scope_sx / 2 + dx * (i), +scope_sy / 2)
-                    porch_column_pre.NodeRefs.append(objOsmGeom.AddNode(getID(), Lat, Lon))
-
-                    # 5
-                    porch_column_pre.NodeRefs.append(node_no_1)
-
-                    porch_column_pre.updateBBox(objOsmGeom)
-
-                    Objects2.append(porch_column_pre)
-                    blnThereAreUnprocessedRules = True
+                Objects2.extend(new_objects)
+                blnThereAreUnprocessedRules = True
 
             elif osmObject.getTag("building:part") == "porch_column_pre":
                 osmObject.osmtags["building:colour"] = "green"
-                # insert_circle
-                # Определить bbox
-                # определить радиус и центр окружности.
-                # вычислить координаты точек окружности
-                # Вставить ноды в списки нодов
-                # Вставить новый вей
-                # Добавить на него теги
-
-                #insert circle
-                porch_column = T3DObject()
-                porch_column.id = getID()
-                porch_column.type = "way"
-
-                porch_column.osmtags["building:part"] = "porch_column"
-                porch_column.osmtags["height"] = osmObject.getTag("height")
-                porch_column.osmtags["min_height"]=osmObject.getTag("min_height")
-
-                porch_column.osmtags["building:colour"]=osmObject.getTag("building:colour")
-
-                R = osmObject.size/3
-                Lat = [None] * 12
-                Lon = [None] * 12
-                ids = [None] * 12
-
-                for i in range(12):
-                    alpha = 2 * pi / 12 * i
-
-                    Lat[i], Lon[i]=osmObject.localXY2LatLon(R * cos(alpha), R * sin(alpha))
-                    ids[i] = getID()
-                    #print(ids[i], x[i], y[i])
-                    objOsmGeom.AddNode(ids[i], Lat[i], Lon[i])
-                    intNodeNo = objOsmGeom.FindNode(ids[i])
-                    porch_column.NodeRefs.append(intNodeNo)
-                #objOsmGeom.AddNode(ids[0], Lat[0], Lon[0])
-                intNodeNo = objOsmGeom.FindNode(ids[0])
-                porch_column.NodeRefs.append(intNodeNo)
-
-                Objects2.append(porch_column)
-
+            
+                new_objects=insert_circle(osmObject, objOsmGeom)
+                Objects2.extend(new_objects)
+            
             elif osmObject.getTag("building:part") == "porch_top":
                 osmObject.osmtags["building:colour"] = "blue"
                 Objects2.append(osmObject)
@@ -186,5 +219,7 @@ def main():
         Objects = Objects2
 
     writeOsmXml(objOsmGeom, Objects , "D:\\rewrite.osm")
+
+
 
 main()
