6d51e7223dce689c6196d847ef5597517dfa21cf
Kirill "Zkir <mekillot@yandex.ru>
2020-07-25 04:15:49 +0300

    Yandex metrika added

--- 3dcheck/building.py
+++ 3dcheck/building.py
@@ -163,6 +163,20 @@ def CreateObjectPage(strQuadrantName,cells, intObjectIndex):
     print( '    <script src="/js/classie.js"></script>'+ '\n')
     print( '    <script src="/js/demo1.js"></script>'+ '\n')
 
+    print('''<!-- Yandex.Metrika counter -->
+             <script type="text/javascript" >
+             (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+             m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+             ym(65563393, "init", {
+             clickmap:true,
+             trackLinks:true,
+             accurateTrackBounce:true
+             });
+             </script>
+             <noscript><div><img src="https://mc.yandex.ru/watch/65563393" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+             <!-- /Yandex.Metrika counter -->''')
+
     print( '</body>'+ '\n')
     print( '</html>'+ '\n')
 
--- 3dcheck/region.py
+++ 3dcheck/region.py
@@ -202,6 +202,20 @@ def CreateRegionSummaryPage(strQuadrantName, strInputFile, blnCreateObjectPages,
     print( '<p>Дата формирования страницы: ' + getTimeStamp() + '</p>' + '\n')
     #zero frame for josm links
     print( '<div style="display: none;"><iframe name="josm"></iframe></div>' + '\n')
+    print('''<!-- Yandex.Metrika counter -->
+             <script type="text/javascript" >
+             (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+             m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+             ym(65563393, "init", {
+             clickmap:true,
+             trackLinks:true,
+             accurateTrackBounce:true
+             });
+             </script>
+             <noscript><div><img src="https://mc.yandex.ru/watch/65563393" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+             <!-- /Yandex.Metrika counter -->''')
+
     print( '</body>'+ '\n')
     print( '<html>' + '\n')
 
