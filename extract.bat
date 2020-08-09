1c9a7621505e5dcbdfcd21bc499926f82b735231
Kirill "Zkir <mekillot@yandex.ru>
2020-08-10 02:58:35 +0300

    The list of processed object extended.

--- extract.bat
+++ extract.bat
@@ -24,9 +24,10 @@ osmfilter %SOURCE_FILE% --keep="landuse=religious and barrier=fence" >%WORK_FOLD
 osmfilter %SOURCE_FILE% --keep="building:part=*" >%WORK_FOLDER%\building_parts.osm
 osmfilter %SOURCE_FILE% --keep="building=* and historic=*" >%WORK_FOLDER%\historic.osm
 osmfilter %SOURCE_FILE% --keep="building=* and wikipedia=*" >%WORK_FOLDER%\wikipedia.osm
+osmfilter %SOURCE_FILE% --keep="building=* and tourism=attraction" >%WORK_FOLDER%\attractions.osm
 
 rem osmfilter %SOURCE_FILE% --keep-nodes="place=*" --keep-ways= --keep-relations=  >%WORK_FOLDER%\geocoder.osm
 rem osmfilter %SOURCE_FILE% --keep="boundary=administrative"   >%WORK_FOLDER%\geocoder1.osm
 
-call osmosis --rx %WORK_FOLDER%\churches.osm --rx %WORK_FOLDER%\towers.osm --rx %WORK_FOLDER%\water_tower.osm --rx %WORK_FOLDER%\walls.osm --rx %WORK_FOLDER%\walls2.osm --rx %WORK_FOLDER%\church_fences.osm --rx %WORK_FOLDER%\wikipedia.osm --merge --merge --merge --merge --merge --merge --wx %WORK_FOLDER%\objects-all.osm
+call osmosis --rx %WORK_FOLDER%\churches.osm --rx %WORK_FOLDER%\towers.osm --rx %WORK_FOLDER%\water_tower.osm --rx %WORK_FOLDER%\walls.osm --rx %WORK_FOLDER%\walls2.osm --rx %WORK_FOLDER%\church_fences.osm --rx %WORK_FOLDER%\wikipedia.osm --rx %WORK_FOLDER%\historic.osm --rx %WORK_FOLDER%\attractions.osm  --merge --merge --merge --merge --merge --merge --merge --merge --wx %WORK_FOLDER%\objects-all.osm
 call osmosis --rx %WORK_FOLDER%\objects-all.osm --rx %WORK_FOLDER%\building_parts.osm  --merge --wx %WORK_FOLDER%\objects-with-parts.osm
