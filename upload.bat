8c5c5dd0ab8262f4696b10a3e07df5e7e2bf10f0
Kirill "Zkir <mekillot@yandex.ru>
2024-08-13 03:25:41 +0300

    ftp excluded. it is not used really anymore

--- upload.bat
+++ upload.bat
@@ -6,18 +6,13 @@ echo Working folder: %WORK_FOLDER%
 
 xcopy /Y /Q %WORK_FOLDER%\30_3dmodels\*.x3d  d:\_VFR_LANDMARKS_3D_RU\3dcheck\models
 xcopy /Y /Q %WORK_FOLDER%\30_3dmodels\*.png  d:\_VFR_LANDMARKS_3D_RU\3dcheck\models
-rem xcopy /Y /Q %WORK_FOLDER%\30_3dmodels\*.html d:\_VFR_LANDMARKS_3D_RU\3dcheck\models
 
-xcopy /Y /Q %WORK_FOLDER%\10_osm_extracts\*.dat d:\_VFR_LANDMARKS_3D_RU\3dcheck\data
+xcopy /Y /Q %WORK_FOLDER%\11_osm_objects_list\*.dat d:\_VFR_LANDMARKS_3D_RU\3dcheck\data
 xcopy /Y /Q %WORK_FOLDER%\Quadrants.dat d:\_VFR_LANDMARKS_3D_RU\3dcheck\data
 
 
-"c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u %WORK_FOLDER%\30_3dmodels\*.x3d   -p /http/models -s
-"c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u %WORK_FOLDER%\30_3dmodels\*.png   -p /http/models -s
-rem "c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u %WORK_FOLDER%\30_3dmodels\*.html   -p /http/models -s
-
-rem "c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u d:\_VFR_LANDMARKS_3D_RU\3dcheck\%QUADRANT%.html   -p /http/ -s
-"c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u d:\_VFR_LANDMARKS_3D_RU\3dcheck\index.html   -p /http/ -s
-
-"c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u %WORK_FOLDER%\10_osm_extracts\*.dat -p /http/data -s
-"c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u %WORK_FOLDER%\Quadrants.dat   -p /http/data -s 
+rem "c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u %WORK_FOLDER%\30_3dmodels\*.x3d   -p /http/models -s
+rem "c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u %WORK_FOLDER%\30_3dmodels\*.png   -p /http/models -s
+rem "c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u d:\_VFR_LANDMARKS_3D_RU\3dcheck\index.html   -p /http/ -s
+rem "c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u %WORK_FOLDER%\11_osm_objects_list\*.dat -p /http/data -s
+rem "c:\Program Files\CoreFTP\coreftp.exe" -site 3dcheck -O -u %WORK_FOLDER%\Quadrants.dat   -p /http/data -s 
