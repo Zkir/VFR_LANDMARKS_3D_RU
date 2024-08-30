43b8b5a5d1a8aa28af1b0248c1fa615707b6272b
Kirill "Zkir <mekillot@yandex.ru>
2024-08-31 01:25:23 +0300

    protection from running twice

--- run_validator.bat
+++ run_validator.bat
@@ -1,2 +1,19 @@
+@echo off
+set lockfile=work_folder\.lock
+rem ========== protection from running the process twice ===
+IF EXIST %lockfile% goto error
+touch %lockfile%
+pause 
+del %lockfile%
+rem ========== main part ===================================
+
 make planet-update
 make 
+
+rem ========== tail ========================================
+echo process complete 
+goto end
+:error 
+echo process is already running
+
+:end
