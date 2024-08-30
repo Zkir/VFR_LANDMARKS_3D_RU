0bd77c64c7a8363a55ffca2f77c0b7bbbefcf717
Kirill "Zkir <mekillot@yandex.ru>
2024-08-31 01:30:35 +0300

    bugfixing

--- run_validator.bat
+++ run_validator.bat
@@ -3,14 +3,13 @@ set lockfile=work_folder\.lock
 rem ========== protection from running the process twice ===
 IF EXIST %lockfile% goto error
 touch %lockfile%
-pause 
-del %lockfile%
 rem ========== main part ===================================
 
 make planet-update
 make 
 
 rem ========== tail ========================================
+del %lockfile%
 echo process complete 
 goto end
 :error 
