2298e72d54d1236fc7a4bb634fd0051243ff6b83
Kirill "Zkir <mekillot@yandex.ru>
2024-08-31 17:13:14 +0300

    more quite output

--- scripts/buildings_extract-per-region.bat
+++ scripts/buildings_extract-per-region.bat
@@ -1,14 +1,11 @@
 @echo off
-echo Updating quadrant data from osm
 
 SET QUADRANT=%1
 SET BBOX=poly\%~2
 set WORK_FOLDER=work_folder\10_osm_extracts\%QUADRANT%
 set PLANET=work_folder\07_building_data
 
-echo Quadrant: %QUADRANT%
-echo BBOX: %BBOX%
-echo Working folder: %WORK_FOLDER%
+echo Updating %QUADRANT% quadrant data from osm
 
 mkdir %WORK_FOLDER%
 
