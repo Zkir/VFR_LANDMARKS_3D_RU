4e978dd7467d7a258985879f31298bb96226e592
Kirill "Zkir <mekillot@yandex.ru>
2024-09-05 02:36:33 +0300

    building osm-data extraction optimized

--- scripts/buildings_extract.bat
+++ scripts/buildings_extract.bat
@@ -1,27 +1,19 @@
 @echo off
-
 set WORK_FOLDER=work_folder\07_building_data
-
 SET SOURCE_FILE="work_folder\00_planet.osm\russia-latest.o5m"
 
-echo Working folder: %WORK_FOLDER%
+rem echo Working folder: %WORK_FOLDER%
+touch %WORK_FOLDER%\.start
 
-osmfilter %SOURCE_FILE% --keep="amenity=place_of_worship building=church =cathedral =bell_tower =chapel =shrine =temple =mosque =synagogue tower:type=bell_tower" >%WORK_FOLDER%\churches.osm
-osmfilter %SOURCE_FILE% --keep="tower:type=defensive =fortification" >%WORK_FOLDER%\towers.osm
-osmfilter %SOURCE_FILE% --keep="man_made=chimney" >%WORK_FOLDER%\chimney.osm
-osmfilter %SOURCE_FILE% --keep="man_made=water_tower" >%WORK_FOLDER%\water_tower.osm
-osmfilter %SOURCE_FILE% --keep="building=* and barrier=city_wall" >%WORK_FOLDER%\walls.osm
-osmfilter %SOURCE_FILE% --keep="historic=yes and barrier=wall" >%WORK_FOLDER%\walls2.osm
-osmfilter %SOURCE_FILE% --keep="landuse=religious and barrier=fence" >%WORK_FOLDER%\church_fences.osm
+osmfilter %SOURCE_FILE% --keep="building=* and ( amenity=place_of_worship or historic=* or wikipedia=* or tourism=attraction or barrier=city_wall or height>90 )" >%WORK_FOLDER%\buildings.osm
+osmfilter %SOURCE_FILE% --keep="building=church =cathedral =bell_tower =chapel =shrine =temple =mosque =synagogue tower:type=bell_tower" >%WORK_FOLDER%\churches.osm
+osmfilter %SOURCE_FILE% --keep="( man_made=water_tower ) or ( tower:type=defensive =fortification )" >%WORK_FOLDER%\towers.osm
+osmfilter %SOURCE_FILE% --keep="( landuse=religious and barrier=fence ) or ( historic=yes and barrier=wall )" >%WORK_FOLDER%\walls2.osm
 osmfilter %SOURCE_FILE% --keep="building:part=*" >%WORK_FOLDER%\building_parts.osm
-osmfilter %SOURCE_FILE% --keep="building=* and historic=*" >%WORK_FOLDER%\historic.osm
-osmfilter %SOURCE_FILE% --keep="building=* and wikipedia=*" >%WORK_FOLDER%\wikipedia.osm
-osmfilter %SOURCE_FILE% --keep="building=* and tourism=attraction" >%WORK_FOLDER%\attractions.osm
-osmfilter %SOURCE_FILE% --keep="building=* and height>90" >%WORK_FOLDER%\tall.osm
 
+rem chimneys are not used now, but they maybe potentially needed for x-plane, since those are tall objects-all
+rem osmfilter %SOURCE_FILE% --keep="man_made=chimney" >%WORK_FOLDER%\chimney.osm
 
-rem osmfilter %SOURCE_FILE% --keep-nodes="place=*" --keep-ways= --keep-relations=  >%WORK_FOLDER%\geocoder.osm
-rem osmfilter %SOURCE_FILE% --keep="boundary=administrative"   >%WORK_FOLDER%\geocoder1.osm
-
-call osmosis --rx %WORK_FOLDER%\churches.osm --rx %WORK_FOLDER%\towers.osm --rx %WORK_FOLDER%\water_tower.osm --rx %WORK_FOLDER%\walls.osm --rx %WORK_FOLDER%\walls2.osm --rx %WORK_FOLDER%\church_fences.osm --rx %WORK_FOLDER%\wikipedia.osm --rx %WORK_FOLDER%\historic.osm --rx %WORK_FOLDER%\attractions.osm --rx %WORK_FOLDER%\tall.osm  --merge --merge --merge --merge --merge --merge --merge --merge --merge --wx %WORK_FOLDER%\objects-all.osm
+call osmosis --rx %WORK_FOLDER%\buildings.osm --rx %WORK_FOLDER%\churches.osm --rx %WORK_FOLDER%\towers.osm --rx %WORK_FOLDER%\walls2.osm  --merge --merge --merge --wx %WORK_FOLDER%\objects-all.osm
 call osmosis --rx %WORK_FOLDER%\objects-all.osm --rx %WORK_FOLDER%\building_parts.osm  --merge --wx %WORK_FOLDER%\objects-with-parts.osm
+touch %WORK_FOLDER%\.fin
