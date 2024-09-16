29f9e83d4e4112eaba85fa50a9f65896a0cca00c
Kirill "Zkir <mekillot@yandex.ru>
2024-09-16 16:04:11 +0300

    resent changes page should include red errors also

--- OsmParser/make_tops.py
+++ OsmParser/make_tops.py
@@ -18,8 +18,8 @@ DB_FOLDER="d:\\_VFR_LANDMARKS_3D_RU\\work_folder\\22_all_osm_objects_list\\"
 strInputFile = DB_FOLDER+"all-objects.dat"
 rsObjectList = loadDatFile(strInputFile)
 
+# top-200
 rsObjectList.sort(key=lambda row: safe_int(row[QUADDATA_NUMBER_OF_PARTS]), reverse=True)
-
 rsOutput=[]
 n = 0  
 for rec1 in rsObjectList:
@@ -30,12 +30,12 @@ for rec1 in rsObjectList:
 
 saveDatFile(rsOutput,DB_FOLDER+'RUS_TOP.dat')
 
-
+# Recent changes
 rsObjectList.sort(key=lambda row: str(row[QUADDATA_LAST_UPDATE_DATE]).strip(), reverse=True)
 rsOutput=[]
 n = 0        
 for rec1 in rsObjectList:
-    if (rec1[QUADDATA_OSM3D] == 'True') and n <200:
+    if (rec1[QUADDATA_NUMBER_OF_PARTS] != '0') and n <200:
         rsOutput.append(rec1)
         n += 1
 
