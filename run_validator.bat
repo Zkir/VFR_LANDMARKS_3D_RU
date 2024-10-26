d04f7a775ad9554355aae7e660421c7c2c7df7e6
Kirill "Zkir <mekillot@yandex.ru>
2024-10-26 03:19:35 +0300

    error handling in validator script

--- run_validator.bat
+++ run_validator.bat
@@ -1,19 +1,26 @@
 @echo off
 set lockfile=work_folder\.lock
 rem ========== protection from running the process twice ===
-IF EXIST %lockfile% goto error
+IF EXIST %lockfile% goto error_already_running
 touch %lockfile%
 rem ========== main part ===================================
 
 make planet-update > work_folder\log.txt 2>&1
+if errorlevel 1 goto error
 make   >> work_folder\log.txt 2>&1
+if errorlevel 1 goto error
 
 rem ========== tail ========================================
 del %lockfile%
 echo process complete >> work_folder\log.txt 2>&1
 echo process complete 
 goto end
-:error 
+
+:error_already_running
 echo process is already running
+goto end
+
+:error 
+echo error has occured. sorry. 
 
 :end
